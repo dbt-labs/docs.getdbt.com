d7ada16bdae411d8d135de6c857ddcdd457a4723
Claire Carroll <carroll.claire.e@gmail.com>
2020-02-03 10:24:42 -0500

    Mess around with mdx-deck

---
+++ website/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+            },
+        ],
+    },
+}
