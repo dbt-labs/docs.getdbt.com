c15811ce850ad3878f5e9da6e18af0b31c0133c3
Claire Carroll <carroll.claire.e@gmail.com>
2020-02-04 16:30:13 -0500

    Add SQL that was used to setup tutorial

---
+++ website/docs/learn/load_data.sql
@@ -0,0 +1,32 @@
+create schema raw.jaffle_shop;
+
+create table raw.jaffle_shop.customers
+(
+    id integer,
+    first_name varchar,
+    last_name varchar
+);
+
+create table raw.jaffle_shop.orders
+(
+  id integer,
+  user_id integer,
+  order_date date,
+  status varchar
+);
+
+copy into raw.jaffle_shop.customers from 's3://dbt-tutorial-test/customers.csv'
+    file_format = (
+        type = 'CSV'
+        field_delimiter = ','
+        skip_header = 1
+    )
+;
+
+copy into raw.jaffle_shop.orders from 's3://dbt-tutorial-test/orders.csv'
+    file_format = (
+        type = 'CSV'
+        field_delimiter = ','
+        skip_header = 1
+    )
+;
