50573c9c2e443ac8fe6cc5055caf5c5fae7f8a08
David Krevitt <77362861+krevitt@users.noreply.github.com>
2021-09-23 12:37:54 -0500

    Jason's recommendations for wistia fix

--- website/src/components/wistia/index.js
+++ website/src/components/wistia/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 
 function WistiaVideo({id}) {
   return (
-	 <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;">
-		<div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
-			<iframe 
-				src="https://fast.wistia.net/embed/iframe/${id}?seo=false&videoFoam=true"  
-				allow="autoplay; fullscreen" 
-				allowtransparency="true" 
-				frameborder="0" 
-				scrolling="no" 
-				class="wistia_embed" 
-				name="wistia_embed" allowfullscreen msallowfullscreen width="100%" height="100%"></iframe>
+
+  	 <React.Fragment>
+		 <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;">
+			<div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
+				<iframe 
+					src={`https://fast.wistia.net/embed/iframe/${id}?seo=false&videoFoam=true`} 
+					allow="autoplay; fullscreen" 
+					allowtransparency="true" 
+					frameborder="0" 
+					scrolling="no" 
+					class="wistia_embed" 
+					name="wistia_embed" allowfullscreen msallowfullscreen width="100%" height="100%">
+				</iframe>
+			</div>
 		</div>
-	</div>
-	<script src="https://fast.wistia.net/assets/external/E-v1.js" async></script>
+		<script src="https://fast.wistia.net/assets/external/E-v1.js" async></script>
+  	 </React.Fragment>
 	);
 }
 
