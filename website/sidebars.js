9f089acb0cb89cea6a7da2e8ec4788f9494af86e
Mark McDonald <mmcdonald@clearcover.com>
2021-11-10 12:27:45 -0600

    fixing branch

--- website/sidebars.js
+++ website/sidebars.js
@@ -67,7 +67,6 @@ module.exports = {
           type: "category",
           label: "Migration guides",
           items: [
-            "docs/guides/migration-guide/upgrading-to-1-0-0",
             "docs/guides/migration-guide/upgrading-to-0-21-0",
             "docs/guides/migration-guide/upgrading-to-0-20-0",
             "docs/guides/migration-guide/upgrading-to-0-19-0",
