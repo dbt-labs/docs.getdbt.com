187913fca491879972b2c8b361d5824e25362098
Jason Karlavige <jkarlavige@gmail.com>
2022-02-09 16:19:52 -0500

    handle localStorage and version url param

--- website/src/stores/VersionContext.js
+++ website/src/stores/VersionContext.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from "react"
+import React, { useState, useEffect, createContext } from "react"
 import { versions } from '../../dbt-versions.json'
 
 const lastReleasedVersion = versions[0];
@@ -10,14 +10,41 @@ const VersionContext = createContext({
 
 export const VersionContextProvider = ({ children }) => {
 
-  const [version, setVersion] = useState(lastReleasedVersion)
+  const [version, setVersion] = useState('')
+
+  useEffect(() => {
+    const storageVersion = window.localStorage.getItem('dbtVersion')
+    const { search } = window.location
+    const urlParams = new URLSearchParams(search);
+    const versionParam = urlParams.get('version')
+
+    if(versionParam && versions.includes(versionParam)) {
+      {/* 
+        * Check if version param exists in url,
+        * and is in current versions array
+        * If true, set version to param value
+      */}
+      setVersion(versionParam)
+      window.localStorage.setItem('dbtVersion', versionParam)
+    } else {
+      {/*
+        * If localStorage version exists, set version to LS value
+        * Otherwise set version to latest version 
+      */}
+      storageVersion
+        ? setVersion(storageVersion)
+        : setVersion(lastReleasedVersion)
+    }
+  }, [])
 
   const updateVersion = (e) => {
     if(!e.target)
       return
     
     const versionValue = e.target.text.replace('v', '')
-    setVersion(versionValue)
+    versionValue &&
+      setVersion(versionValue)
+      window.localStorage.setItem('dbtVersion', versionValue)
   }
 
   const context = {
