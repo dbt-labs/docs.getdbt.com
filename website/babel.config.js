170a410e8687e3e26932db782956e6127acd7615
Jason Karlavige <jkarlavige@gmail.com>
2022-03-02 12:46:03 -0500

    initial testing setup

---
+++ website/babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    require.resolve('@docusaurus/core/lib/babel/preset'),
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ],
+};
---
+++ website/src/components/versionBlock/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react';
+import VersionBlock from './index'
+
+describe("Test VersionBlock component", () => {
+  it('Should render same text passed into title prop', () => { 
+    render(<VersionBlock>This is a test</VersionBlock>)
+    const testElement = screen.getByText(/this is a test/i)
+    expect(testElement).toBeInTheDocument()
+  })
+})
