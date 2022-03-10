7752e44ca73bf48527bee7cae5ae164f7ec5658d
Jason Karlavige <jkarlavige@gmail.com>
2022-03-10 08:49:44 -0500

    remove demo content from defer and comment out 1.1 version

--- website/dbt-global-variables.js
+++ website/dbt-global-variables.js
@@ -1,14 +1,19 @@
+/*
+ * Full Documentation at: 
+ * https://www.notion.so/dbtlabs/Versioning-on-Docusaurus-c6a4a41a66cd4ea2970854cc42cb5b70#1803b9cb666442e5ac8885cf0bba321f
+ *
+ */ 
+
 exports.dbtVariables = {
-  dbtCore: {
-    name: "dbt Core",
+  // Example global variable with versioning
+  // If version 0.21 or lower is selected
+  // "Old Example String" will replace "Example String"
+  exampleString: {
+    name: "Example String",
     versions: [
       {
-        "name": "dbt Core Lite",
-        "version": "1.0"
-      },
-      {
-        "name": "dbt Lite",
-        "version": "0.20"
+        "name": "Old Example String",
+        "version": "0.21"
       }
     ]
   } 
