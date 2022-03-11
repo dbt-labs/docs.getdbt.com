ae5b3ccdb7f9df86f4182e5f1ae29dc288760fd8
Jason Karlavige <jkarlavige@gmail.com>
2022-03-11 11:00:50 -0500

    add test file for version nav component

--- website/src/theme/NavbarItem/VersionsNavItem.test.js
+++ website/src/theme/NavbarItem/VersionsNavItem.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react';
 import { fail } from 'assert';
-import DropdownNavbarItem from './index'
+import VersionsNavItem from './index'
 
 describe("Test DropdownNavbarItem component", () => {
   it('Should render new version when updateVersion method hit', () => {
-    render(<DropdownNavbarItem>Test versioned content</DropdownNavbarItem>)
+    render(<VersionsNavItem>Test versioned content</VersionsNavItem>)
     fail('Failed')
   })
 })
