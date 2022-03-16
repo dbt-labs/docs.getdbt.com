69ea300c2b5bbfa5a65c7513efd1911499da2cab
Jason Karlavige <jkarlavige@gmail.com>
2022-03-16 10:57:04 -0400

    additional tests in progress

--- website/src/components/versionBlock/index.test.js
+++ website/src/components/versionBlock/index.test.js
@@ -2,23 +2,24 @@ import React from 'react'
 import { render, screen } from '@testing-library/react';
 import { fail } from 'assert';
 import VersionBlock from './index'
+import { VersionContextProvider } from '../../stores/VersionContext'
 
 describe("Test VersionBlock component", () => {
   it('Should render same text passed into title prop', () => { 
-    render(<VersionBlock>This is a test</VersionBlock>)
-    const testElement = screen.getByText(/this is a test/i)
+    render(<VersionBlock>Versioned content</VersionBlock>)
+    const testElement = screen.getByText(/Versioned content/i)
     expect(testElement).toBeInTheDocument()
-    screen.debug()
   })
 
-  it('Should render content when version from context is undefined', () => {
-    render(<VersionBlock>Test versioned content</VersionBlock>)
-    const content = screen.getByText(/Test versioned content/i)
+  it('Should not render content if current version below firstVersion', () => {
+    render(
+      <VersionContextProvider>
+        <VersionBlock firstVersion="0.99">
+          Versioned content
+        </VersionBlock>
+      </VersionContextProvider>
+    )
+    const content = screen.getByText('Versioned content')
     expect(content).toBeInTheDocument()
-  })
-
-  it('Should render content when both firstVersion or lastVersion props are undefined', () => {
-    render(<VersionBlock>Test versioned content</VersionBlock>)
-    fail('Failed')
   })
 })
