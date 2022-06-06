36eb3c2c4865f86f82841fdc6db4e9ef6b48fbbf
Anders <anders.swanson@dbtlabs.com>
2022-06-06 10:20:10 -0700

    add sidebar link to Mindsdb config

--- website/sidebars.js
+++ website/sidebars.js
@@ -307,6 +307,7 @@ const sidebarSettings = {
         "reference/resource-configs/materialize-configs",
         "reference/resource-configs/firebolt-configs",
         "reference/resource-configs/teradata-configs",
+        "reference/resource-configs/mindsdb-configs"
       ],
     },
     {
