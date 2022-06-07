27d9f8f572e8325378a226a3d499e54aae4d15cb
Anders <anders.swanson@dbtlabs.com>
2022-06-07 13:15:08 -0700

    Update website/sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -308,7 +308,7 @@ const sidebarSettings = {
         "reference/resource-configs/firebolt-configs",
         "reference/resource-configs/teradata-configs",
         "reference/resource-configs/clickhouse-configs",
-        "reference/resource-configs/mindsdb-configs"
+        "reference/resource-configs/mindsdb-configs",
         "reference/resource-configs/mssql-configs",
         "reference/resource-configs/azuresynapse-configs",
         "reference/resource-configs/greenplum-configs",
