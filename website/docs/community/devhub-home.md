22017850061309784c067080889383c3e8b99c5c
Joel Labes <joel.labes@dbtlabs.com>
2022-09-01 21:28:44 +1200

    Add interesting and unanswered discussions on devhub home

--- website/docs/community/devhub-home.md
+++ website/docs/community/devhub-home.md
@@ -1,3 +1,16 @@
 # Welcome to the dbt Developer Hub! 
 
-<DiscourseTopicList templateStyle="complete" numResults="10" createNew="true"/>
+<table>
+<tr>
+<th>Interesting Discussions</th>
+<th>Unanswered Questions; Can you help?</th>
+</tr>
+<tr>
+<td width="500px">
+    <DiscourseTopicList templateStyle="complete" numResults="10" createNew="true"/>
+</td>
+<td width="500px">
+    <DiscourseTopicList templateStyle="complete" numResults="10" createNew="true" topPeriod="weekly"/>
+</td>
+</tr>
+</table>
--- website/src/components/discourseTopicList/index.js
+++ website/src/components/discourseTopicList/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function DiscourseTopicList({ categoryID, numResults = 3, templateStyle = "basic", createNew = false, orderBy = "default" }) {
-  var source = `https://discourse.getdbt.com/embed/topics?template=${templateStyle}&top_period=quarterly&category=${categoryID}&order=views&per_page=${numResults}${createNew ? '&allow_create=true' : ''}&order=${orderBy}`;
+function DiscourseTopicList({ categoryID, numResults = 3, templateStyle = "basic", createNew = false, orderBy = "default", topPeriod = "quarterly" }) {
+  var source = `https://discourse.getdbt.com/embed/topics?template=${templateStyle}&top_period=${topPeriod}&category=${categoryID}&order=views&per_page=${numResults}${createNew ? '&allow_create=true' : ''}&order=${orderBy}`;
 
   return (
     <iframe src={source} width="100%" height="1400px" />
