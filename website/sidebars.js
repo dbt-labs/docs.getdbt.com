136ea6e991e6321de81f3199efae7e0f5939a7e9
mirnawong1 <89008547+mirnawong1@users.noreply.github.com>
2022-09-12 14:46:51 +0100

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -73,7 +73,16 @@ const sidebarSettings = {
       label: "Frequently asked questions",
       link: {
         type: 'generated-index', title: 'Frequently asked questions',
-        description: 'Our Frequently Asked Questions (FAQs) section is a space where you can find an answer to some questions we get asked a lot (but that we’re happy to answer!). If you have a question or are still stuck on something, just reach out to us by emailing support@getdbt.com or clicking on the chat widget, and we’ll do our best to help out.', slug: '/docs/faqs',
+        description: 'In these frequently asked questions, you will find answers to common questions. If you have a question or need clarity, you can always reach out to support by emailing [support@getdbt.com](mailto:support@getdbt.com) or using the chat widget in dbt Cloud, and we'll do our best to help out. **What is dbt?**
+dbt enables analytics engineers to transform data in their warehouses by simply writing select statements. dbt handles turning these select statements into tables and views.dbt does the T in ELT (Extract, Load, Transform) processes – it doesn’t extract or load data, but it’s extremely good at transforming data that’s already loaded into your warehouse. The role of dbt within a modern data stack is discussed in more detail [here](https://blog.fishtownanalytics.com/what-exactly-is-dbt-47ba57309068).
+dbt also enables analysts to work more like software engineers, in line with the dbt Viewpoint.
+
+**What is the difference between dbt Core, the dbt CLI and dbt Cloud?**
+dbt Core is the software that takes a [dbt project](https://docs.getdbt.com/docs/building-a-dbt-project/projects) (.sql and .yml files) and a command and then creates tables/views in your warehouse. dbt Core includes a command line interface (CLI) so that users can execute dbt commands using a terminal program. dbt Core is [open source](https://github.com/dbt-labs/dbt) and free to use.
+dbt Cloud is an application that helps teams use dbt. dbt Cloud provides a web-based IDE to develop dbt projects, a purpose-built scheduler, and a way to share dbt documentation with your team. dbt Cloud offers a number of features for free, as well as additional features in paid tiers (check out the [pricing here](https://www.getdbt.com/pricing/)).
+
+**dbt on demand courses**
+Want to learn dbt in more detail? The dbt Labs team has published [five online courses](https://courses.getdbt.com/collections) to level up your analytics engineering skills on your own time. ', slug: '/docs/faqs',
       },
       items: [
         {
