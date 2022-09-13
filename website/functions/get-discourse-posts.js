3fde615b36bf9692f36b2ff559895b91df56b5ae
Jason Karlavige <jkarlavige@gmail.com>
2022-09-13 15:40:36 -0400

    add netlify function to get discourse posts

---
+++ website/functions/get-discourse-posts.js
@@ -0,0 +1,45 @@
+const axios = require('axios')
+
+async function getDiscoursePosts() {
+  const { DISCOURSE_API_KEY , DISCOURSE_USER } = process.env
+
+  try {
+    // Set API endpoint and headers
+    let discourse_endpoint = `https://discourse.getdbt.com`
+    let headers = {
+      'Accept': 'application/json',
+      'Api-Key': DISCOURSE_API_KEY,
+      'Api-Username': DISCOURSE_USER,
+    }
+
+    // Get events from Discourse
+    let { data } = await axios.get(`${discourse_endpoint}/posts`, { headers })
+    console.log('data', data)
+    if(!data)
+      throw new Error('Unable to get results from api request.')
+
+    // Return posts 
+    return await returnResponse(200, data)
+  } catch(err) {
+    // Log and return the error
+    console.log('err', err)
+    return await returnResponse(500, { error: 'Unable to get events from Discourse.'})
+  }
+}
+
+async function returnResponse(status, res) {
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS' 
+  }
+  const resObj = {
+    statusCode: status,
+    headers,
+    body: JSON.stringify(res)
+  }
+  return resObj
+}
+
+exports.handler = getDiscoursePosts
