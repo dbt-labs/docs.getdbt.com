d3c2e266d3acebe29469694b3fe9bb68576307d7
Jason Karlavige <jkarlavige@gmail.com>
2022-09-13 16:08:18 -0400

    add styling with demo content

--- website/src/components/discourse/index.js
+++ website/src/components/discourse/index.js
@@ -5,7 +5,84 @@ export default function DiscourseFeed(styles = {}) {
   return (
     <section className={feedStyles.discourseSection}>
       <h1>dbt Discourse</h1>
-      Content
-    </section >
+      <div className={feedStyles.discoursePosts}>
+        <h2>Latest posts</h2>
+        <ul>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+        </ul>
+        <a class={`button button--primary ${feedStyles.discourseCta}`} href="https://discourse.getdbt.com/" title="See all latest posts" target="_blank">See all latest posts</a>
+      </div>
+      <div className={feedStyles.discoursePosts}>
+        <h2>Fun title</h2>
+        <ul>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+        </ul>
+        <a class={`button button--primary ${feedStyles.discourseCta}`} href="https://discourse.getdbt.com/" title="See all latest posts" target="_blank">See all latest posts</a>
+      </div>
+      <div className={feedStyles.discoursePosts}>
+        <h2>Another fun title</h2>
+        <ul>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+          <li>
+            <a href="#" title="title">Local Variable/set variable usage in config block</a>
+            <span> - by username</span>
+          </li>
+        </ul>
+        <a class={`button button--primary ${feedStyles.discourseCta}`} href="https://discourse.getdbt.com/" title="See all latest posts" target="_blank">See all latest posts</a>
+      </div>
+    </section>
   )
 }
