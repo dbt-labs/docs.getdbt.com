fd0d486e8efc6305e72434a45518fbd843c53405
Jason Karlavige <jkarlavige@gmail.com>
2022-09-15 14:34:39 -0400

    adjust styles

--- website/src/pages/forum.js
+++ website/src/pages/forum.js
@@ -7,7 +7,7 @@ function Events() {
   return (
     <Layout>
       <div className="container discourse-forum-page">
-        <DiscourseFeed title="Open topics" category='help' status='unsolved' after='2022-08-14' post_count={10} />
+        <DiscourseFeed title="Open topics" category='help' status='unsolved' after='2022-08-14' />
         <DiscourseFeed title="Cool Problem Solving" category='show-and-tell' after='2022-06-14' inString='first' />
         <DiscourseFeed title="Interesting Discussions" category='discussions' inString='first' after='2022-06-14' />
       </div>
