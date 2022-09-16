f568f0981ef95524913109b80ec7f2a33981a3c0
David Krevitt <77362861+krevitt@users.noreply.github.com>
2022-09-16 12:16:49 -0500

    Description of events page to 1 line

--- website/src/pages/events.js
+++ website/src/pages/events.js
@@ -10,9 +10,7 @@ function Events() {
         <div className="container events-page">
            <section>
             <h1>Upcoming dbt Community Events</h1>
-            <p>Join us for upcoming meetups, conferences, or office hours with the dbt Labs team.</p>
-
-            <p>Events are online unless explicitly listed as in-person.</p>
+            <p>Join us for upcoming meetups, conferences, or office hours with the dbt Labs team. Events are online unless explicitly listed as in-person.</p>
 
             <EventsFeed />
            </section>
