a3acba4ea8d55769d453eb7580e835d1292d1de4
speedwaycollective <113943763+speedwaycollective@users.noreply.github.com>
2022-09-19 16:34:20 -0400

    Update index.js

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -278,6 +278,7 @@ function Home() {
           </div>
         </div>
         <div className="banner-animation" dangerouslySetInnerHTML={getBanner()}></div>
+        <p>test</p>
       </Layout>
     </>
   );
