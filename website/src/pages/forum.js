805f11743d153041712e7691e4ece6f051eaf68d
Jason Karlavige <jkarlavige@gmail.com>
2022-09-20 10:54:37 -0400

    remove status for show-and-tell and discussions category components

--- website/src/pages/forum.js
+++ website/src/pages/forum.js
@@ -14,8 +14,8 @@ function Events() {
         <div className='container'>   
           <h1>dbt Discourse</h1>
           <DiscourseFeed title='Open topics' category='help' status='unsolved'  link_text='See open topics' link_href='https://discourse.getdbt.com/c/help/19' show_cta={true} />
-          <DiscourseFeed title='Cool Problem Solving' category='show-and-tell' after='2022-06-14' status='unsolved' link_text='See more topics' link_href='https://discourse.getdbt.com/c/show-and-tell/22' show_cta={true} />
-          <DiscourseFeed title='Interesting Discussions' category='discussions'  status='unsolved' link_text='See discussions' link_href='https://discourse.getdbt.com/c/discussions/21' show_cta={true} />
+          <DiscourseFeed title='Cool Problem Solving' category='show-and-tell' link_text='See more topics' link_href='https://discourse.getdbt.com/c/show-and-tell/22' show_cta={true} />
+          <DiscourseFeed title='Interesting Discussions' category='discussions' link_text='See discussions' link_href='https://discourse.getdbt.com/c/discussions/21' show_cta={true} />
         </div>
       </section>
     </Layout>
