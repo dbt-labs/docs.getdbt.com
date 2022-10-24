33aa7724d2fa6ff63c695d1f1393e2c4cd4b597e
glin <glin@macbook-pro.myfiosgateway.com>
2022-10-24 18:01:24 -0400

    shorten survey CTA copy

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -64,7 +64,7 @@ var siteSettings = {
     announcementBar: {
       id: "live_qa",
       content:
-        "Got opinions on analytics engineering? Tell us in the 5-minute dbt Community Survey.",
+        "Take the 5-minute dbt Community Survey!",
       backgroundColor: "#047377",
       textColor: "#fff",
       isCloseable: true,
