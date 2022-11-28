ac6e9d5cfa7d338ead815393fe6bcfd23ba83893
Jason Karlavige <jason.karlavige@dbtlabs.com>
2022-11-28 14:53:09 -0500

    adjust styles and meta tags

--- website/src/components/community/index.js
+++ website/src/components/community/index.js
@@ -6,16 +6,35 @@ import Layout from '@theme/Layout';
 import Hero from '@site/src/components/hero';
 import { CommunitySpotlightCard } from './CommunitySpotlightCard'
 
+const communityTitle = 'Community spotlight'
+const communityDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+
+
+
 function CommunitySpotlightList({ spotlightData }) {
   console.log('spotlightData', spotlightData)
+  const { siteConfig } = useDocusaurusContext()
+
+  // Build meta title from communityTitle and docusaurus config site title
+  const metaTitle = `${communityTitle}${siteConfig?.title ? ` | ${siteConfig.title}` : ''}`
 
   return (
     <Layout>
-      <Hero heading="Community spotlight" subheading="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam." showGraphic={false} customStyles={{marginBottom: 0}} />
+      <Head>
+        <title>{metaTitle}</title>
+        <meta property="og:title" content={metaTitle} />
+        <meta property="og:description" content={communityDescription} />
+      </Head>
+      <Hero 
+        heading={communityTitle} 
+        subheading={communityDescription} 
+        showGraphic={false} 
+        customStyles={{marginBottom: 0}} 
+      />
       <section id='spotlight-members-section'>
         <div className='container'>   
-          {spotlightData.map(member => (
-            <CommunitySpotlightCard frontMatter={member.data} />
+          {spotlightData.map((member, i) => (
+            <CommunitySpotlightCard frontMatter={member.data} key={i} />
           ))}
         </div>
       </section>
