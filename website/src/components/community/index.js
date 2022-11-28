524883f4c91430346c3f87ad8468b84db901c40f
Jason Karlavige <jason.karlavige@dbtlabs.com>
2022-11-28 14:01:30 -0500

    adjust hero component to hide graphic if false

--- website/src/components/community/index.js
+++ website/src/components/community/index.js
@@ -3,22 +3,22 @@ import Head from '@docusaurus/Head';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import { ThemeClassNames } from '@docusaurus/theme-common';
+import Hero from '@site/src/components/hero';
 import { CommunitySpotlightCard } from './CommunitySpotlightCard'
 
 function CommunitySpotlightList({ spotlightData }) {
   console.log('spotlightData', spotlightData)
 
   return (
-    <Layout
-      wrapperClassName={ThemeClassNames.wrapper.docsPages}
-      pageClassName={ThemeClassNames.page.docsDocPage}
-    >
-      <>
-        {spotlightData.map(member => (
-          <CommunitySpotlightCard frontMatter={member.data} />
-        ))}
-      </>
+    <Layout>
+     <section id='community-spotlight-page'>
+        <Hero heading="Welcome to the dbt Developer Hub" subheading="Your home base for learning dbt, connecting with the community and contributing to the craft of analytics engineering " showGraphic={false} />
+        <div className='container'>   
+          {spotlightData.map(member => (
+            <CommunitySpotlightCard frontMatter={member.data} />
+          ))}
+        </div>
+      </section>
     </Layout>
   )
 }
--- website/src/components/hero/index.js
+++ website/src/components/hero/index.js
@@ -7,7 +7,9 @@ function Hero({ heading, subheading, showGraphic = false }) {
   const { isDarkTheme } = useColorMode();
   return (
     <header className={` ${styles.Hero} container-fluid`}>
-      <div className={styles.showGraphic}></div>
+      {showGraphic && (
+        <div className={styles.showGraphic}></div>
+      )}
       <div className={`container`}>
         <div className="row justify-content-center">
           <div className="col col--7">
