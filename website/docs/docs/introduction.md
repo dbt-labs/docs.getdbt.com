f8ed126f832e4142a980aa9c4afefd9b5a1370b3
Jason Karlavige <jason.karlavige@dbtlabs.com>
2022-12-06 16:25:47 -0500

    support custom widths

--- website/docs/docs/introduction.md
+++ website/docs/docs/introduction.md
@@ -7,8 +7,6 @@ id: "introduction"
 
 <Snippet src="what-is-dbt-intro" />
 
-wee fun time <Lightbox src="/img/hamburger-icon.png" alt="Test" collapsed={false} /> and other content here
-
 dbt compiles and runs your analytics code against your data platform, enabling you and your team to collaborate on a single source of truth for metrics, insights, and business definitions. This single source of truth, combined with the ability to define tests for your data, reduces errors when logic changes, and alerts you when issues arise.
 
 Read more about why we want to enable analysts to work more like software engineers in [The dbt Viewpoint](/community/resources/viewpoint).
--- website/src/components/lightbox/index.js
+++ website/src/components/lightbox/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-function Lightbox({src, alt = undefined, title = undefined, collapsed = false}) {
+function Lightbox({
+  src, 
+  collapsed = false, 
+  alt = undefined, 
+  title = undefined, 
+  width = undefined
+}) {
   return (
     <>
       <link href="/css/featherlight-styles.css" type="text/css" rel="stylesheet" />
-      <div className={ `${styles.docImage} ${collapsed ? styles.collapsed : ''}` }>
+      <div 
+        className={`${styles.docImage} ${collapsed ? styles.collapsed : ''}`}
+        style={width && {maxWidth: width}}
+      >
         <span>
           <a href="#" data-featherlight={src}>
             <img
