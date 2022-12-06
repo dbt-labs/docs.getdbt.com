a6c91a4dd7ef3bce7933c2858225c491fd48d32b
Jason Karlavige <jason.karlavige@dbtlabs.com>
2022-12-06 15:38:15 -0500

    adjust props

--- website/src/components/lightbox/index.js
+++ website/src/components/lightbox/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-function Lightbox({children, src, title, collapsed}) {
-  var collapsedClass = !!collapsed ? styles.collapsed : '';
+function Lightbox({src, alt = undefined, title = undefined, collapsed = false}) {
+  const collapsedClass = !!collapsed ? styles.collapsed : '';
 
   return (
     <>
       <link href="/css/featherlight-styles.css" type="text/css" rel="stylesheet" />
       <div className={ `${styles.docImage} ${collapsedClass}` }>
-          <span>
-              <a href="#" data-featherlight={src}>
-                  <img
-                      data-toggle="lightbox"
-                      alt={title}
-                      src={src}
-                  />
-              </a>
-          </span>
+        <span>
+          <a href="#" data-featherlight={src}>
+            <img
+              data-toggle="lightbox"
+              alt={alt ? alt : title ? title : ''}
+              src={src}
+            />
+          </a>
+        </span>
+        {title && (
           <p className={styles.title}>{ title }</p>
+        )}
       </div>
     </>
   );
