4f2790f4ea259f44023ddbec784aed526a35d385
Andrew Sweet <andrewmsweet@gmail.com>
2022-12-13 09:26:34 -0500

    Replace errors with warnings

--- website/src/components/lineage/index.js
+++ website/src/components/lineage/index.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
-import('@dbt-labs/react-dbt-dag/dag.css');
-import('@dbt-labs/react-dbt-dag/dag.standalone.css');
-
-/** As a private package, not every devleoper will have access to this repo. */
-const DagExports = import('@dbt-labs/react-dbt-dag');
-
 let Dag = null;
 
-DagExports.then((resolved) => {
-    Dag = resolved.Dag;
-});
+try {
+    /** As a private package, not every developer will have access to this repo. */
+    const DagImport = require('@dbt-labs/react-dbt-dag');
+    require('@dbt-labs/react-dbt-dag/dag.css');
+    require('@dbt-labs/react-dbt-dag/dag.standalone.css');
+
+    Dag = DagImport.Dag;
+} catch (err) {
+    /**
+     * react-dbt-dag is a private repo. Not all developers of the
+     * open source docs will have access to this repo.
+     */
+}
 
 export const Lineage = ({ dagNodes, currentNodeId }) => {
     if (!Dag) {
