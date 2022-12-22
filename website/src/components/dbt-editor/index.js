da24f9a7ab0062ce32e66e350a326768f4e3fe5f
Jason Karlavige <jkarlavige@gmail.com>
2022-12-22 13:08:58 -0500

    removed unused sub-menu file

--- website/src/components/dbt-editor/index.js
+++ website/src/components/dbt-editor/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Editor from "@monaco-editor/react";
-import SubMenu from './sub-menu';
 import { Lineage } from '../lineage';
 import { buildSidebar } from './utils/build-sidebar';
 import { parseCsv } from './utils/parse-csv';
