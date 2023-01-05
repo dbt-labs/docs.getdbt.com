92dbbfeb2967ca80df4b6474b7d9ad498f48018b
john-rock <johnmrock.jr@gmail.com>
2023-01-05 15:02:30 -0500

    start building

--- website/src/pages/report-a-problem.js
+++ website/src/pages/report-a-problem.js
@@ -1,18 +1,32 @@
-
-import React from 'react';
-import Layout from '@theme/Layout';
-import Hero from '@site/src/components/hero';
+import React from "react";
+import Layout from "@theme/Layout";
+import Hero from "@site/src/components/hero";
+import { HubSpotForm } from "@site/src/components/hubspotForm";
 
 function ReportAProblem() {
-  
   return (
     <>
       <Layout>
-        <div className="container container--fluid home" style={{ "padding": "0", "background": "#FFF" }}>
-          <Hero heading="Welcome to the dbt Developer Hub" subheading="Your home base for learning dbt, connecting with the community and contributing to the craft of analytics engineering " showGraphic />
-            <section>
-                <h1>Test</h1>
-            </section>
+        <div
+          className="container container--fluid home"
+          style={{ padding: "0", background: "#FFF" }}
+        >
+          <Hero
+            heading="Report a problem"
+            subheading="Lorem ipsum, report a problem about the docs site subtext belongs here."
+            showGraphic
+          />
+          <section className="report-a-problem">
+            <div>
+              <HubSpotForm
+                region="na1"
+                portalId="21770202"
+                formId="51549be8-d959-419b-950d-bd2b085942fc"
+                sfdcCampaignId="7014v0000022cGhAAI"
+                containerId="report-hubspot-form"
+              />
+            </div>
+          </section>
         </div>
       </Layout>
     </>
