d2876791acc3311072d512bb0971d51380975a09
Jason Karlavige <jkarlavige@gmail.com>
2023-01-18 15:10:31 -0500

    use live url

--- website/plugins/buildRSSFeeds/index.js
+++ website/plugins/buildRSSFeeds/index.js
@@ -2,9 +2,7 @@ const fs = require('fs')
 const Feed = require('feed').Feed
 const { getDirectoryFiles } = require('../buildGlobalData/get-directory-files')
 
-// const siteUrl = 'https://docs.getdbt.com'
-const siteUrl = 'https://deploy-preview-2713--docs-getdbt-com.netlify.app'
-// const siteUrl = 'http://localhost:3000'
+const siteUrl = 'https://docs.getdbt.com'
 
 module.exports = function buildRSSFeedsPlugin(context, options) {
   return {
@@ -32,9 +30,9 @@ module.exports = function buildRSSFeedsPlugin(context, options) {
         copyright: `Copyright © ${today.getFullYear()} dbt Labs™, Inc. All Rights Reserved.`,
         updated: new Date(2023, 1, 18),
         feedLinks: {
-          rss2: `${siteUrl}/rss.xml`,
-          atom: `${siteUrl}/atom.xml`,
-          json: `${siteUrl}/rss.json`,
+          rss2: `http://localhost:3000/rss.xml`,
+          atom: `http://localhost:3000/atom.xml`,
+          json: `http://localhost:3000/rss.json`,
         }
       });
 
