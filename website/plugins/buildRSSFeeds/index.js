094100db766836308a7684af85811d4e422f0156
Jason Karlavige <jkarlavige@gmail.com>
2023-01-18 12:50:11 -0500

    build url

--- website/plugins/buildRSSFeeds/index.js
+++ website/plugins/buildRSSFeeds/index.js
@@ -13,37 +13,11 @@ module.exports = function buildRSSFeedsPlugin(context, options) {
       // Release Notes directory
       const releaseNotesDirectory = 'docs/docs/dbt-versions/release-notes'
 
-      // Get all files within all release notes directories
-      const releaseNotesFiles = getDirectoryFiles(releaseNotesDirectory)
+      // Get all files and file data within all release notes directories
+      const releaseNotesFiles = getDirectoryFiles(releaseNotesDirectory, [], true)
+      console.log('releaseNotesFiles', releaseNotesFiles)
 
-      // Build release note data for feeds
-      const releaseNotesData = releaseNotesFiles.reduce((arr, releaseNote) => {
-        const fileData = fs.readFileSync(
-          releaseNote.filePath, 
-          { encoding: 'utf8' }
-        )
-        
-        if(!fileData)
-          return null
-        
-        // convert frontmatter to json
-        const fileJson = matter(fileData)
-
-        if(!fileJson)
-          return null
-
-        let noteObj = fileJson.data
-
-        // TODO: Get url for current page
-        // and pass to link property
-        noteObj.link = releaseNote.filePath
-
-        arr.push(noteObj)
-
-        return arr
-      }, [])
-      
-      if(!releaseNotesData || !releaseNotesData.length) 
+      if(!releaseNotesFiles || !releaseNotesFiles.length) 
         return null
       
       // Generate RSS feeds
@@ -64,15 +38,23 @@ module.exports = function buildRSSFeedsPlugin(context, options) {
         }
       });
 
-      // Add all release notes to feeds
-      await releaseNotesData.map(note => {
+      // Prepare data and add all release notes to feeds
+      await releaseNotesFiles.map(note => {
+        const { data } = note
+        // console.log('data', data)
+
         // TODO: Set date by date in frontmatter tags array
         let feedItemObj = {
-          title: note.title,
-          date: new Date()
+          title: data.title,
+          date: new Date(),
         }
-        if(note?.id) feedItemObj.id = note.id
-        if(note?.description) feedItemObj.description = note.description
+        if(data?.id) feedItemObj.id = data.id
+        if(data?.description) feedItemObj.description = data.description
+        
+        // Add url field
+        feedItemObj.link = getLink(data)
+
+        console.log('feedItemObj', feedItemObj)
         feed.addItem(feedItemObj)
       })
 
@@ -83,4 +65,21 @@ module.exports = function buildRSSFeedsPlugin(context, options) {
     },
 
   };
+}
+
+function getLink(data) {
+  const { id, file, path } = data
+
+  // Remove extra 'docs/' from path
+  let urlPath = path.replace('docs/docs/', 'docs/')
+
+  // Remove leading number/dash, and file name from release notes directory
+  // For example, the path: docs/dbt-versions/release-notes/12-January-2022/IDE-autocomplete-more.md
+  // is converted to docs/dbt-versions/release-notes/January-2022
+  urlPath = urlPath.replace(/release-notes\/(.\d\-)(.*[a-zA-Z]\-)(\d{4})(.*.md)/g, 'release-notes/$2$3')
+
+  // If frontmatter 'id' set, use as page name, otherwise revert to file name
+  return id
+    ? `${siteUrl}/${urlPath}/${id}`
+    : `${siteUrl}/${urlPath}/${file.replace('.md', '')}`
 }
