f742784717de70a95b0359a8189000434b69d72f
john-rock <johnmrock.jr@gmail.com>
2023-01-31 13:54:49 -0500

    refactor querying discourse by external_id

--- website/functions/get-discourse-comments.js
+++ website/functions/get-discourse-comments.js
@@ -3,6 +3,7 @@ const axios = require('axios')
 const { DISCOURSE_DEVBLOG_API_KEY , DISCOURSE_USER_SYSTEM } = process.env
 const DEVBLOG_URL = 'https://docs.getdbt.com/blog/'
 const DISCOURSE_TOPIC_ID = 2
+const DISCOURSE_EXTERNAL_ID_SUFFIX = '-testing2'
 
 // Set API endpoint and headers
 let discourse_endpoint = `https://discourse.getdbt.com`
@@ -12,71 +13,45 @@ let headers = {
   'Api-Username': DISCOURSE_USER_SYSTEM,
 }    
 
-async function getDiscourseComments( event ) {
+async function getDiscourseComments(event) {
+  let postTitle, postSlug, topicId, comments;
+
   try {
-    let postTitle = event.queryStringParameters.title.trim()
-    let postSlug = event.queryStringParameters.slug
-    let postTitleEncoded = encodeURIComponent(postTitle)
+    postTitle = event.queryStringParameters.title.trim();
+    postSlug = event.queryStringParameters.slug;
 
-    if(!postTitle) throw new Error('Unable to query Discourse API.')
+    if (!postSlug) throw new Error("Unable to query Discourse API.");
 
-    let topics = await searchDiscourseTopics(postTitleEncoded)
-    
-    let allTopics = topics
-    let allTopicTitles = []
-    let topicExists = false
-    
-    let topicId = null
-    let comments = []
+    topicId = await searchDiscourseExternalId(postSlug);
 
-    // Return all the topic titles from Discourse
-    if(topics && topics.length > 0 ) {
-        allTopicTitles = allTopics.reduce((topicsArr, topic) => {
-            topicsArr.push(topic.title)
-            return topicsArr
-        }, [])
-
-        // Check to see if the Dev Blog post title matches any of the Discourse topic titles
-        topicExists = allTopics.some(topic => topic.title === postTitle)
-    }
-
-    console.log('topicExists', topicExists)
-
-    // If it does not exist in Discourse, create a new topic
-    if(!topicExists) {
-        topicId = await createDiscourseTopic(postTitle, postSlug)
-
-        if(topicId) {
-            comments = await getDiscourseTopicbyID(topicId)
-        }
-     
-        // Remove the the first post of the comments array since it is not a comment
-        if (comments.length) {
-            comments.shift()
-            comments = { topicId, comments }
-        }
-
-        return await returnResponse(200, comments)
+    // First check if the dev blog post exists in Discourse
+    // Get the comments if it does
+    if (typeof topicId === "number") {
+      comments = await getDiscourseTopicbyID(topicId);
     } else {
-        // Set topicId to the result from allTopics that matches the postTitle
-        topicId = await getTopicId(allTopics, postTitle)
+      // If the dev blog post does not exist in Discourse
+      // Create a new topic and get the comments
+      topicId = await createDiscourseTopic(postTitle, postSlug);
+      if (typeof topicId === "number") {
+        comments = await getDiscourseTopicbyID(topicId);
+        comments.shift();
+        comments = { topicId, comments };
 
-        if(topicId) {
-            comments = await getDiscourseTopicbyID(topicId)
-        }
-
-        // Remove the the first post of the comments array since it is not a comment
-        if (comments.length) {
-            comments.shift()
-            comments = { topicId, comments }
-        }
-
-        return await returnResponse(200, comments)
+        return await returnResponse(200, comments);
+      } else {
+        return await returnResponse(500, {
+          error: "Unable to create Discourse topic TopicID is not a number.",
+        });
+      }
     }
 
-  } catch(err) {
-    console.log('err', err)
-    return await returnResponse(500, { error: 'Unable to get topics from Discourse.'})
+    comments.shift();
+    comments = { topicId, comments };
+
+    return await returnResponse(200, comments);
+  } catch (err) {
+    console.log("err", err);
+    return await returnResponse(500, {error: "Unable to get topics from Discourse."});
   }
 }
 
@@ -89,42 +64,40 @@ async function createDiscourseTopic(title, slug) {
             raw: `This is a companion discussion topic for the original entry at ${DEVBLOG_URL}${slug}`,
             category: DISCOURSE_TOPIC_ID,
             embed_url: `${DEVBLOG_URL}${slug}`,
-            external_id: `${slug}`
+            external_id: `${slug}${DISCOURSE_EXTERNAL_ID_SUFFIX}`
         }, { headers })
 
-        const topicId = response?.data?.topic_id
+        let topicId = await response.data.topic_id
 
         console.log('Topic successfully created with topic_id', topicId)
+        console.log(response)
 
         return topicId
     
     } catch(err) {
-        console.log('err', err)
+        console.log('err', err.response)
         return await returnResponse(500, { error: 'Unable to create Discourse topic.'})
     }
 }
 
 async function getDiscourseTopicbyID(topicId) {
     console.log(`Topic found setting topic id - ${topicId}`)
-
     try {
         let { data: { post_stream } } = await axios.get(`${discourse_endpoint}/t/${topicId}.json`, { headers })
         return post_stream.posts
     } catch(err) {
-        console.log('err', err)
+        console.log('err', err.response)
         return await returnResponse(500, { error: 'Unable to get Discourse topic by ID.'})
     }
 }
 
-async function searchDiscourseTopics(title) {
-    console.log(`Searching for topic in Discourse - ${title}`)
-
+async function searchDiscourseExternalId(slug) {
+    console.log(`Searching for external_id in Discourse - ${slug}`);
     try {
-        let { data: { topics } } = await axios.get(`${discourse_endpoint}/search?q=${title}&in:title`, { headers })
-        return topics
-    } catch(err) {
-        console.log('err', err)
-        return await returnResponse(500, { error: 'Unable to search Discourse topics.'})
+        const data = await axios.get(`${discourse_endpoint}/t/external_id/${slug}${DISCOURSE_EXTERNAL_ID_SUFFIX}.json`, { headers });
+        return data.data.id;
+    } catch (err) {
+        return await returnResponse(500, { error: 'Unable to search Discourse topics.' });
     }
 }
 
@@ -141,16 +114,6 @@ async function returnResponse(status, res) {
     body: JSON.stringify(res)
   }
   return resObj
-}
-
-async function getTopicId(allTopics, postTitle) {
-    allTopics.forEach(topic => {
-        if(topic.title === postTitle) {
-            topicId = topic.id
-        }
-    })
-
-    return topicId
 }
 
 exports.handler = getDiscourseComments
