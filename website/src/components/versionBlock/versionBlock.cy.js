48b0cc326603c9335ba5f1a95f63dfa2dde8c4d6
wendyvasq <wendy.vasquez@dbtlabs.com>
2023-02-17 13:45:03 -0800

    version Block test improvements

--- website/src/components/versionBlock/versionBlock.cy.js
+++ website/src/components/versionBlock/versionBlock.cy.js
@@ -1,41 +1,31 @@
 import React from 'react'
 import VersionBlock from './index.js'
-// import { mount } from '@cypress/react'
-// import VersionContextProvider from '../../stores/VersionContext.js'
+import VersionContext from '../../stores/VersionContext.js'
 
 describe('Test VersionBlock component', () => {
   it('Should render same text passed into title prop', () => {
     cy.mount(<VersionBlock>Versioned content</VersionBlock>)
     cy.contains('Versioned content').should('be.visible')
   })
-  // Developer note:
-  // Need to figure out how to mount muliple components
-  // -----------------
-  // const testData = [
-  //   {
-  //     desc: 'Should render content if current version >= firstVersion',
-  //     version: "0.21",
-  //     shouldShowContent: true
-  //   },
-  //   {
-  //     desc: 'Should not render content if current version less than firstVersion',
-  //     version: "0.20",
-  //     shouldShowContent: false
-  //   },
-  // ]
-  // testData.forEach((testCase) => {
-  //   it(testCase.desc, () => {
-  //     localStorage.setItem('dbtVersion', testCase.version)
-  //     const firstVersion = "0.21"
+  it('Should render content if current version >= firstVersion', () => {
+    cy.mount(
+      <VersionContext.Provider value={{ version: '1.0' }}>
+        <VersionBlock firstVersion={'0.9'}>
+          Current version
+        </VersionBlock>
+      </VersionContext.Provider>
+    )
+    cy.contains('Current version').should('exist')
+  })
 
-  //     mount(
-  //       <VersionContextProvider>
-  //         <VersionBlock firstVersion={firstVersion}>
-  //           Current version
-  //         </VersionBlock>
-  //       </VersionContextProvider>
-  //     )
-  //     cy.contains('Current version').should('be.visible')
-  //   })
-  // })
+  it('Should not render content if current version less than firstVersion', () => {
+    cy.mount(
+      <VersionContext.Provider value={{ version: '1.0' }}>
+        <VersionBlock firstVersion={'1.1'}>
+          Current version
+        </VersionBlock>
+      </VersionContext.Provider>
+    )
+    cy.contains('Current version').should('not.exist')
+  })
 })
