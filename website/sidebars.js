ce6b68853c0f16d83e5bef0e25181b1a474fe4f3
mirnawong1 <mirna.wong@fishtownanalytics.com>
2023-02-20 14:31:50 +0000

    updates

--- website/sidebars.js
+++ website/sidebars.js
@@ -209,9 +209,9 @@ const sidebarSettings = {
           type: "category",
           label: "dbt Cloud production jobs",
           items: [
+            "docs/deploy/configure-jobs",
             "docs/deploy/artifacts",
             "docs/deploy/webhooks",
-            "docs/deploy/job-commands",
             "docs/deploy/job-triggers",
             "docs/deploy/job-notifications",
             "docs/deploy/source-freshness",
