d3a0233a70ed829987cba43b10e439ac678b6ad4
john-rock <johnmrock.jr@gmail.com>
2023-04-26 13:38:12 -0400

    dont build spotlight index page

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -240,7 +240,7 @@ var siteSettings = {
     path.resolve("plugins/customWebpackConfig"),
     [path.resolve("plugins/buildGlobalData"), { versionedPages, versionedCategories }],
     path.resolve("plugins/buildAuthorPages"),
-    path.resolve("plugins/buildSpotlightIndexPage"),
+    //path.resolve("plugins/buildSpotlightIndexPage"),
     path.resolve("plugins/buildQuickstartIndexPage"),
     path.resolve("plugins/buildRSSFeeds"),
   ],
