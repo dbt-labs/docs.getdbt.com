5fecd7b80d7c8498f900aeaa2bc6637253831f55
wendyvasq <wendy.vasquez@dbtlabs.com>
2023-05-10 19:26:49 -0700

    update conditional

--- website/src/components/docCarousel/index.js
+++ website/src/components/docCarousel/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Children } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Navigation } from 'swiper';
 import 'swiper/css/navigation';
 
-
 function DocCarousel({ children }) {
-    console.log(children)
+    if ( !Children.toArray(children).length > 0 ){      
+        return false
+    }
     return (
         <div className='docswiper'>
             <Swiper
