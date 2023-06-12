dfb09e7de68b3a7d2afa7b0582de8be2e44c6bc8
Mitchell <mitchellbarker@outlook.com>
2023-06-12 14:57:20 -0500

    update to use browseronly

--- website/src/pages/dbt-cloud/api-v2.js
+++ website/src/pages/dbt-cloud/api-v2.js
@@ -1,18 +1,24 @@
 import React from "react";
 import Layout from "@theme/Layout";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 import { API } from "@stoplight/elements";
 import "@stoplight/elements/styles.min.css";
 import useBaseUrl from "@docusaurus/useBaseUrl";
+
 function dbtCloudAPI() {
   return (
     <Layout permalink="/">
-      <API
-        //   todo: update this url
-        apiDescriptionUrl="https://raw.githubusercontent.com/dbt-labs/dbt-cloud-openapi-spec/mbarker/generated-docs/openapi-v2.yaml"
-        platformUrl={useBaseUrl("/")}
-        basePath={useBaseUrl("/dbt-cloud/api-v2") + "#"}
-      />
+      <BrowserOnly fallback={<div>Loading...</div>}>
+        {() => (
+          <API
+            //   todo: update this url
+            apiDescriptionUrl="https://raw.githubusercontent.com/dbt-labs/dbt-cloud-openapi-spec/mbarker/generated-docs/openapi-v2.yaml"
+            platformUrl={useBaseUrl("/")}
+            basePath={useBaseUrl("/dbt-cloud/api-v2") + "#"}
+          />
+        )}
+      </BrowserOnly>
     </Layout>
   );
 }
--- website/src/pages/dbt-cloud/api-v3.js
+++ website/src/pages/dbt-cloud/api-v3.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "@theme/Layout";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 import { API } from "@stoplight/elements";
 import "@stoplight/elements/styles.min.css";
@@ -8,12 +9,16 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 function dbtCloudAPI() {
   return (
     <Layout permalink="/">
-      <API
-        //   todo: update this url
-        apiDescriptionUrl="https://raw.githubusercontent.com/dbt-labs/dbt-cloud-openapi-spec/mbarker/generated-docs/openapi-v3.yaml"
-        platformUrl={useBaseUrl("/")}
-        basePath={useBaseUrl("/dbt-cloud/api-v3") + "#"}
-      />
+      <BrowserOnly fallback={<div>Loading...</div>}>
+        {() => (
+          <API
+            //   todo: update this url
+            apiDescriptionUrl="https://raw.githubusercontent.com/dbt-labs/dbt-cloud-openapi-spec/mbarker/generated-docs/openapi-v3.yaml"
+            platformUrl={useBaseUrl("/")}
+            basePath={useBaseUrl("/dbt-cloud/api-v3") + "#"}
+          />
+        )}
+      </BrowserOnly>
     </Layout>
   );
 }
