792c7c02c2441b46df5f8039ebeda5cde13cd404
john-rock <johnmrock.jr@gmail.com>
2023-06-14 10:17:50 -0400

    add icon support on quickstart card

--- website/src/components/quickstartGuideCard/index.js
+++ website/src/components/quickstartGuideCard/index.js
@@ -1,9 +1,29 @@
 import React from "react";
 import Link from "@docusaurus/Link";
 import styles from "./styles.module.css";
+import { useColorMode } from "@docusaurus/theme-common";
 
 function QuickstartGuideCard({ frontMatter }) {
-  const { id, title, time_to_complete } = frontMatter;
+  const { id, title, time_to_complete, icon } = frontMatter;
+  const { colorMode } = useColorMode();
+
+  const renderIcon = () => {
+    if (icon.startsWith('fa-')) {
+      return <i className={`fa ${icon} ${styles.icon}`} />;
+    } else {
+      return (
+        <img
+          src={
+            colorMode === "dark"
+              ? `/img/icons/white/${icon}.svg`
+              : `/img/icons/${icon}.svg`
+          }
+          alt=""
+          className={`${styles.icon}`}
+        />
+      )
+    }
+  }
 
   return (
     <Link
@@ -11,6 +31,8 @@ function QuickstartGuideCard({ frontMatter }) {
       frontMatter={frontMatter}
       className={styles.quickstartCard}
     >
+      {icon && renderIcon()}
+      
       <h3>{title}</h3>
 
       {time_to_complete && (
