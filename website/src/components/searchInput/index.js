e3f512698c440408696b6361f944f33581a67bb6
john-rock <johnmrock.jr@gmail.com>
2023-10-24 09:27:15 -0400

    add search icon

--- website/src/components/searchInput/index.js
+++ website/src/components/searchInput/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import styles from './styles.module.css';
+import React from "react";
+import styles from "./styles.module.css";
 
-const SearchInput = ({ value, onChange, placeholder = "Search...", ...props }) => {
+const SearchInput = ({
+  value,
+  onChange,
+  placeholder = "Search...",
+  ...props
+}) => {
   return (
-    <input
-      type="text"
-      value={value}
-      onChange={(e) => onChange && onChange(e.target.value)}
-      placeholder={placeholder}
-      {...props}
-      className={styles.inputContainer}
-    />
+    <label for="search-input" className={styles.inputContainer}>
+      <i class="fa-regular fa-magnifying-glass"></i>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onChange && onChange(e.target.value)}
+        placeholder={placeholder}
+        {...props}
+        className={styles.input}
+        id="search-input"
+      />
+    </label>
   );
 };
 
