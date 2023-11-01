4ec7ff6f064c11adbbce95a8e7a372e4c3a987ed
mirnawong1 <89008547+mirnawong1@users.noreply.github.com>
2023-11-01 17:46:22 +0000

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -936,9 +936,8 @@ const sidebarSettings = {
     {
       type: "category",
       label: "Database Permissions",
-      // link: { type: "doc", id: "reference/database-permissions/about-database-permissions" },
       items: [
-	"reference/database-permissions/about-database-permissions"
+	"reference/database-permissions/about-database-permissions",
         "reference/database-permissions/databricks-permissions",
         "reference/database-permissions/postgres-permissions",
 	"reference/database-permissions/redshift-permissions",
