6fe2e772c5a62bcaafc56868d2caea3ee4366ef8
mirnawong1 <mirna.wong@fishtownanalytics.com>
2023-11-09 15:41:09 +0000

    updates

--- website/static/js/headerLinkCopy.js
+++ website/static/js/headerLinkCopy.js
@@ -3,19 +3,23 @@
 window.addEventListener("load", function () {
     // Get all the headers with anchor links
     const headers = document.querySelectorAll("h2.anchor, h3.anchor"); 
-  console.log("headers",headers);
+
     headers.forEach((header) => {
       header.style.cursor = "pointer";
       const clipboard = new ClipboardJS(header, { 
         text: function(trigger) {
-          const anchorLink = trigger.getAttribute('href');
-          return window.location.href.split('#')[0] + anchorLink;
+          const anchorLink = trigger.getAttribute("id");
+          return window.location.href.split('#')[0] +'#' + anchorLink;
         }
       });
   
       clipboard.on('success', function(e) {
         // Provide user feedback (e.g., alert or tooltip) here
-        alert("Link copied to clipboard: " + e.text);
+        console.log("e",e);
+        e.trigger.classList.add("clicked");
+        setTimeout(() => {
+          e.trigger.classList.remove('clicked')
+        }, 5000) 
       });
   
       clipboard.on('error', function(e) {
