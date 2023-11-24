523dedea28e3fb615069042b26c8dd8fa5cb9ead
mirnawong1 <mirna.wong@fishtownanalytics.com>
2023-11-24 05:28:15 -0500

    add null function

--- website/src/components/lifeCycle/index.js
+++ website/src/components/lifeCycle/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import styles from './styles.module.css';
 
+function Null({ status }) {
+    if (status) {
+        return null;
+    }
+    
+    return <span className={styles.lifecycle}>{status}</span>;
+}
+
 export default function Lifecycle(props) {
     return (
         <span className={styles.lifecycle}>{props.status}</span>
