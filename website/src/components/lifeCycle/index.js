10bbcbb33f85f756de49bf133d3eb40a0f46993a
mirnawong1 <mirna.wong@fishtownanalytics.com>
2023-11-25 14:24:28 -0500

    combine

--- website/src/components/lifeCycle/index.js
+++ website/src/components/lifeCycle/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import styles from './styles.module.css';
 
-function Null({ status }) {
-    if (status) {
-        return null;
-    }
+// function Item ({ status }) {
+//     if (status) {
+//         return null;
+//     }
     
-    return <span className={styles.lifecycle}>{status}</span>;
-}
+//     return <span className={styles.lifecycle}>{status}</span>;
+// }
 
 export default function Lifecycle(props) {
+    if (!props.status) {
+        return null;
+    }
     return (
         <span className={styles.lifecycle}>{props.status}</span>
     );
