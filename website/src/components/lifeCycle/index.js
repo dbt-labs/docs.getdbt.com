dd956f98e47fe08b10fc69acc9534b011f894c99
mirnawong1 <mirna.wong@fishtownanalytics.com>
2023-11-27 10:20:54 -0500

    adjust to rem

--- website/src/components/lifeCycle/index.js
+++ website/src/components/lifeCycle/index.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css';
 
-// function Item ({ status }) {
-//     if (status) {
-//         return null;
-//     }
-    
-//     return <span className={styles.lifecycle}>{status}</span>;
-// }
-
 export default function Lifecycle(props) {
     if (!props.status) {
         return null;
