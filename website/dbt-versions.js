214eb6f9db0f3f3759ae0418fb44e64cc0cf3b83
Matt Shaver <60105315+matthewshaver@users.noreply.github.com>
2024-01-09 14:14:40 -0500

    Update website/dbt-versions.js

--- website/dbt-versions.js
+++ website/dbt-versions.js
@@ -28,7 +28,7 @@ exports.versions = [
 exports.versionedPages = [
   {
     "page": "reference/resource-properties/unit-tests",
-    "firstVersion": "1.7",
+    "firstVersion": "1.8",
   },
   {
     "page": "reference/resource-configs/store_failures_as",
