812c64f3977c72540fa205104b4d1e08f527d892
mirnawong1 <mirna.wong@fishtownanalytics.com>
2024-01-25 20:36:45 +0000

    adjust colors and font size

--- website/src/components/lifeCycle/index.js
+++ website/src/components/lifeCycle/index.js
@@ -2,14 +2,21 @@ import React from 'react'
 import styles from './styles.module.css';
 
 const statusColors = {
-  enterprise: '#727d91',
-  team: '#727d91',
-  developer: '#727d91',
+  enterprise: '#f3f4f6',
+  team: '#f3f4f6',
+  developer: '#f3f4f6',
   new: '#047377',
   beta: '#047377',
   ga: '#047377',
   'public preview': '#047377',
 };
+
+const fontColors = {
+    enterprise: '#000000',
+    team: '#000000', 
+    developer: '#000000', 
+    // lifecycle statuses use the css determined font color (white)
+  };
 
 export default function Lifecycle(props) {
   // Check if props.status is an array or a single value
@@ -19,7 +26,8 @@ export default function Lifecycle(props) {
     <>
       {statuses.map((status, index) => {
         const style = {
-          backgroundColor: props.backgroundColor || statusColors[status] || '#047377' // Default to teal if no match
+          backgroundColor: props.backgroundColor || statusColors[status] || '#047377', // default to teal if no match
+          color: fontColors[status] || '#fff' // default font color if no matc
         };
 
         return (
