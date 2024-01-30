fe519e8685505f12580e00394bfe98b0fd312164
mirnawong1 <mirna.wong@fishtownanalytics.com>
2024-01-30 20:20:45 +0000

    remove hover

--- website/src/components/detailsToggle/index.js
+++ website/src/components/detailsToggle/index.js
@@ -3,46 +3,11 @@ import styles from './styles.module.css';
 
 function detailsToggle({ children, alt_header = null }) {
   const [isOn, setOn] = useState(false);
-  const [isScrolling, setIsScrolling] = useState(false); // New state to track scrolling
   const [hoverTimeout, setHoverTimeout] = useState(null);
 
   const handleToggleClick = () => {
-    setOn(current => !current); // Toggle the current state
+    setOn(current => !current);
   };
-
-  const handleMouseEnter = () => {
-    if (isOn || isScrolling) return; // Ignore hover if already open or if scrolling
-    const timeout = setTimeout(() => {
-      if (!isScrolling) setOn(true);
-    }, 700); // 
-    setHoverTimeout(timeout);
-  };
-  
-  const handleMouseLeave = () => {
-    if (!isOn) {
-      clearTimeout(hoverTimeout);
-      setOn(false);
-    }
-  };
-
-  const handleScroll = () => {
-    setIsScrolling(true);
-    clearTimeout(hoverTimeout);
-    //setOn(false);
-
-
-    // Reset scrolling state after a  delay
-    setTimeout(() => {
-      setIsScrolling(false);
-    }, 800);
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   useEffect(() => {
     return () => clearTimeout(hoverTimeout);
@@ -53,13 +18,9 @@ function detailsToggle({ children, alt_header = null }) {
       <span 
         className={styles.link} 
         onClick={handleToggleClick}
-        onMouseEnter={handleMouseEnter} 
-        onMouseLeave={handleMouseLeave}
       >
         <span className={`${styles.toggle} ${isOn ? null : styles.toggleUpsideDown}`}></span>&nbsp;
         <span className={styles.headerText}>{alt_header}</span>
-        {/* Visual disclaimer */}
-        <small className={styles.disclaimer}>Hover to view</small>
       </span>
       <div 
         style={{ display: isOn ? 'block' : 'none' }} 
