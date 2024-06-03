1dd167342770187c7131e6b7467b5d16ccd02a53
mirnawong1 <mirna.wong@fishtownanalytics.com>
2024-06-03 13:53:35 +0100

    update

--- website/validate_snippets.py
+++ website/validate_snippets.py
@@ -42,13 +42,13 @@ def fetch_code_snippets(pr_number, repo_owner, repo_name):
         if file['filename'].endswith('.md'):
             patch = file['patch']
             # Extract YAML code snippets from the markdown file, including versioned blocks
-            yaml_snippets = re.findall(r'```yaml(.*?)```', patch, re.DOTALL)
+            yaml_snippets = re.findall(r'```ya?ml(.*?)```', patch, re.DOTALL)
             snippets.extend((file['filename'], snippet) for snippet in yaml_snippets)
     return snippets
 
 # Identify the type of YAML snippet and select the corresponding schema
 def identify_schema(snippet):
-    if "dbt_project.yml" in snippet:
+    if "dbt_project" in snippet:
         return "dbt_project"
     elif "dbt_cloud" in snippet:
         return "dbt_cloud"
