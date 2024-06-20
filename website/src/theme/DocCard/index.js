aafbbf8eff8133446493ddbdecc0b53720188348
Jason Karlavige <jkarlavige@gmail.com>
2024-06-20 16:23:39 -0400

    swizzle latest DocCard component

--- website/src/theme/DocCard/index.js
+++ website/src/theme/DocCard/index.js
@@ -2,52 +2,61 @@ import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import {
-  findFirstCategoryLink,
+  findFirstSidebarItemLink,
   useDocById,
 } from '@docusaurus/theme-common/internal';
+import {usePluralForm} from '@docusaurus/theme-common';
 import isInternalUrl from '@docusaurus/isInternalUrl';
 import {translate} from '@docusaurus/Translate';
+import Heading from '@theme/Heading';
 import styles from './styles.module.css';
-
-/* dbt Customizations:
- * Add styles.glossaryCard to CardContainer
- * Add hoverSnippet prop to CardLayout
- * Prevent truncate if card links to /terms/ page
- * Show hoverSnippet text instead of description if set 
- * Get hoverSnippet from frontmatter and pass to CardLayout
-*/
-
+function useCategoryItemsPlural() {
+  const {selectMessage} = usePluralForm();
+  return (count) =>
+    selectMessage(
+      count,
+      translate(
+        {
+          message: '1 item|{count} items',
+          id: 'theme.docs.DocCard.categoryDescription.plurals',
+          description:
+            'The default description for a category card in the generated index about how many items this category includes',
+        },
+        {count},
+      ),
+    );
+}
 function CardContainer({href, children}) {
   return (
     <Link
       href={href}
-      className={clsx(
-        'card padding--lg', 
-        styles.cardContainer, 
-        href.includes('/terms/') && styles.glossaryCard
-      )}>
+      className={clsx('card padding--lg', styles.cardContainer)}>
       {children}
     </Link>
   );
 }
-function CardLayout({href, icon, title, description, hoverSnippet}) {
+function CardLayout({href, icon, title, description}) {
   return (
     <CardContainer href={href}>
-      <h2 className={clsx(!href.includes('/terms/') && 'text--truncate', styles.cardTitle)} title={title}>
+      <Heading
+        as="h2"
+        className={clsx('text--truncate', styles.cardTitle)}
+        title={title}>
         {icon} {title}
-      </h2>
+      </Heading>
       {description && (
         <p
-          className={clsx(!href.includes('/terms/') && 'text--truncate', styles.cardDescription)}
-          title={hoverSnippet ? hoverSnippet : description}>
-          {hoverSnippet ? hoverSnippet : description}
+          className={clsx('text--truncate', styles.cardDescription)}
+          title={description}>
+          {description}
         </p>
       )}
     </CardContainer>
   );
 }
 function CardCategory({item}) {
-  const href = findFirstCategoryLink(item);
+  const href = findFirstSidebarItemLink(item);
+  const categoryItemsPlural = useCategoryItemsPlural();
   // Unexpected: categories that don't have a link have been filtered upfront
   if (!href) {
     return null;
@@ -57,38 +66,19 @@ function CardCategory({item}) {
       href={href}
       icon="🗃️"
       title={item.label}
-      description={translate(
-        {
-          message: '{count} items',
-          id: 'theme.docs.DocCard.categoryDescription',
-          description:
-            'The default description for a category card in the generated index about how many items this category includes',
-        },
-        {count: item.items.length},
-      )}
+      description={item.description ?? categoryItemsPlural(item.items.length)}
     />
   );
 }
 function CardLink({item}) {
   const icon = isInternalUrl(item.href) ? '📄️' : '🔗';
   const doc = useDocById(item.docId ?? undefined);
-
-  // dbt custom
-  let hoverSnippet
-  if(item.docId && item.href && item.href.includes('/terms/')) {
-    const file = require(`../../../docs/${item.docId}.md`)
-    if(file) {
-      hoverSnippet = file.frontMatter.hoverSnippet
-    }
-  }
-
   return (
     <CardLayout
       href={item.href}
       icon={icon}
       title={item.label}
-      description={doc?.description}
-      hoverSnippet={hoverSnippet}
+      description={item.description ?? doc?.description}
     />
   );
 }
