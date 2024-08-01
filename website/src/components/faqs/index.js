e6d3b8f9127686eba17206a6ed05cd34d3d8fe69
Jason Karlavige <jkarlavige@gmail.com>
2024-08-01 14:22:38 -0400

    fix faq file import

--- website/src/components/faqs/index.js
+++ website/src/components/faqs/index.js
@@ -1,61 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import Markdown from "markdown-to-jsx";
 import styles from './styles.module.css';
 import { usePluginData } from '@docusaurus/useGlobalData';
 
 function FAQ({ path, alt_header = null }) {
   const [isOn, setOn] = useState(false);
   const [filePath, setFilePath] = useState(path);
-  const [fileContent, setFileContent] = useState({});
 
   // Get all faq file paths from plugin
-  const { faqFiles } = usePluginData('docusaurus-build-global-data-plugin');
+  const { faqFiles } = usePluginData("docusaurus-build-global-data-plugin");
 
   useEffect(() => {
     // Search for faq where frontmatter ID matches path prop
-    const faqFile = faqFiles.find(file => path?.includes(file?.id))
+    const faqFile = faqFiles.find((file) => path?.includes(file?.id));
 
     // If faqFile found with ID, set filePath for this file
     if (faqFile?.id) {
-      let thisPath = faqFile?.filePath?.replace('docs/faqs/', '')
-      thisPath = thisPath?.replace('.md', '')
+      let thisPath = faqFile?.filePath?.replace("docs/faqs/", "");
+      thisPath = thisPath?.replace(".md", "");
       if (thisPath) {
         setFilePath(thisPath);
       }
     }
-  }, [])
+  }, []);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        console.log("filePath", filePath);
-        const file = await import(`../../../docs/faqs/${filePath}.md`)
-        console.log("file", file)
-
-        if (file) {
-          const meta = file.metadata;
-          console.log('meta', meta)
-
-          // const markdown = await fetch(file.default);
-          const markdown = file.default;
-          console.log("markdown", markdown);
-          // const contents = await fetch(markdown?.text())
-          // console.log('contents', contents)
-
-          setFileContent({ meta, contents: markdown })
-        }
-      } catch (err) {
-        return null
-      }
-    }
-    fetchData()
-  }, [filePath])
+  let meta, contents
+  const file = require(`../../../docs/faqs/${filePath}.md`);
+  if (file) {
+    meta = file.metadata;
+    contents = file?.default({});
+  }
 
   const toggleOn = function () {
     setOn(!isOn);
-  }
-
-  console.log("fileContent", fileContent);
+  };
 
   return (
     <div className={styles.faqs}>
@@ -65,16 +42,15 @@ function FAQ({ path, alt_header = null }) {
           style={{ transform: isOn ? "rotateX(0deg)" : "rotateX(180deg)" }}
         ></span>
         <span className={styles.headerText}>
-          {alt_header || (fileContent?.meta && fileContent.meta.title)}
+          {alt_header || (meta?.title && meta.title)}
         </span>
       </span>
-      {fileContent.contents ? (
+      {contents ? (
         <div
           style={{ display: isOn ? "block" : "none" }}
           className={styles.body}
         >
-          {/* <Markdown children={fileContent.contents} /> */}
-          {fileContent.contents}
+          {contents}
         </div>
       ) : null}
     </div>
