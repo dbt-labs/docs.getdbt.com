8187bd257091284f5e2a0010b0a262a44be780fa
Jason Karlavige <jkarlavige@gmail.com>
2024-09-12 11:51:46 -0400

    create util to handle version check

--- website/docs/docs/version-testing.md
+++ website/docs/docs/version-testing.md
@@ -3,14 +3,14 @@ title: "Version Testing"
 id: "version-testing"
 ---
 
-<VersionBlock firstVersion="1.8" lastVersion="1.10">
-  ## first version - 1.8 | last version - 1.10
+<VersionBlock firstVersion="1.6" lastVersion="1.9.1">
+  ## first version - 1.6 | last version - 1.9.1
 </VersionBlock>
 
-<!-- <VersionBlock firstVersion="2.2.1">
-  ## first version - 2.2.1
-</VersionBlock> -->
+<VersionBlock firstVersion="2.1">
+  ## first version - 2.1
+</VersionBlock>
 
-<!-- <VersionBlock firstVersion="1.10">
+ <VersionBlock firstVersion="1.10">
   ## first version - 1.10
-</VersionBlock> -->
+</VersionBlock>
--- website/src/components/versionBlock/index.js
+++ website/src/components/versionBlock/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import VersionContext from '../../stores/VersionContext';
-import { sortVersions } from '../../utils/sort-versions';
+import { availableInCurrentVersion } from '../../utils/available-in-current-version';
 
 export default function VersionBlock({ firstVersion = "0", lastVersion = undefined, children }) {
   const { version } = useContext(VersionContext);
@@ -12,53 +12,13 @@ export default function VersionBlock({ firstVersion = "0", lastVersion = undefin
     version && setLoading(false);
   }, [version]);
 
-  // Uncomment this to set the sortVersions util function
-  // sortVersions([
-  //   "1.11",
-  //   "1.7",
-  //   "2.1",
-  //   "1.8",
-  //   "1.6",
-  //   "2.2.1",
-  //   "1.10",
-  //   "2.0.1",
-  //   "1.9",
-  //   "1.9.1",
-  //   "1.10.1",
-  //   "2.0",
-  // ]);
-
   // Only check version if current version set
   if (version) {
-    // Get versions sorted from earliest to latest
-    const sortedVersions = sortVersions([
-      version,
-      firstVersion,
-      ...(lastVersion ? [lastVersion] : []),
-    ]);
-    // Get index of current version, and first/last version props passed into component
-    const currentVersionIndex = sortedVersions?.indexOf(version);
-    const firstVersionIndex = sortedVersions?.indexOf(firstVersion);
-    const lastVersionIndex = sortedVersions?.indexOf(lastVersion);
-    {
-      /*
-       * If last version set, check if current version greater than last version
-       * Or if current version less than first version
-       * If either is true, hide block
-       * Else, if current version less than first version, hide block
-       */
-    }
-    if (lastVersionIndex >= 0) {
-      if (
-        currentVersionIndex > lastVersionIndex ||
-        currentVersionIndex < firstVersionIndex
-      )
-        return null;
-    } else {
-      if (currentVersionIndex < firstVersionIndex) {
-        return null;
-      }
-    }
+    if (!availableInCurrentVersion(
+      version, 
+      firstVersion, 
+      lastVersion
+    )) return null;
   }
 
   return loading ? null : <>{children}</>;
