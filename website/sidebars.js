076fed14ddc22425b717f38b68fe1114f909d284
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2024-12-04 12:38:02 -0600

    Add batch_size, begin, and lookback to sidebar

--- website/sidebars.js
+++ website/sidebars.js
@@ -924,6 +924,8 @@ const sidebarSettings = {
           items: [
             "reference/resource-configs/access",
             "reference/resource-configs/alias",
+            "reference/resource-configs/batch-size",
+            "reference/resource-configs/begin",
             "reference/resource-configs/database",
             "reference/resource-configs/enabled",
             "reference/resource-configs/event-time",
@@ -932,6 +934,7 @@ const sidebarSettings = {
             "reference/resource-configs/grants",
             "reference/resource-configs/group",
             "reference/resource-configs/docs",
+            "reference/resource-configs/lookback",
             "reference/resource-configs/persist_docs",
             "reference/resource-configs/pre-hook-post-hook",
             "reference/resource-configs/schema",
