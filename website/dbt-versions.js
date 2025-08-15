d7cd58eb7c64e33974babee480bc2b072cff0915
Jeremy Yeo <jeremyyeo@users.noreply.github.com>
2025-08-15 16:03:24 +1200

    Update dbt-versions.js

--- website/dbt-versions.js
+++ website/dbt-versions.js
@@ -20,13 +20,13 @@ exports.versions = [
     isPrerelease: true,
   },
   {
-    version: "1.10",
-    customDisplay: "Latest (dbt Core v1.10)",
+    version: "1.11",
+    customDisplay: "Latest (dbt Core v1.11)",
     EOLDate: "2026-06-15",
   },
   {
-    version: "1.9",
-    customDisplay: "Compatible (dbt Core v1.9)",
+    version: "1.10",
+    customDisplay: "Compatible (dbt Core v1.10)",
     EOLDate: "2025-12-08",
   },
 ];
