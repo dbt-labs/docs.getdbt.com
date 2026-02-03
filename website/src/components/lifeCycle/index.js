b63544a3676b4a9c5b00c45088d9012002a5630d
Matt Shaver <60105315+matthewshaver@users.noreply.github.com>
2026-02-02 23:25:52 -0500

    Removing core/fusion lifecycle

--- website/src/components/lifeCycle/index.js
+++ website/src/components/lifeCycle/index.js
@@ -29,8 +29,6 @@ const statusColors = {
   ga: '#ff9e5f',
   preview: '#FE6703',
   private_preview: '#FE6703',
-  core: '#E5E7EB',
-  fusion: '#FE6703',
   // new_constant: '#99A1AF', use this gray color if you want a new color.
 };
 
@@ -46,8 +44,6 @@ const fontColors = {
   private: "#030711",
   private_beta: "#030711",
   private_preview: "#030711",
-  fusion: "#030711",
-  core: "#030711",
 };
 
 // Display names for status values
@@ -62,8 +58,6 @@ const statusDisplayNames = {
   ga: 'GA',
   preview: 'Preview',
   private_preview: 'Private preview',
-  fusion: `Fusion`,
-  core: `Core`,
 };
 
 // URL mapping for predefined lifecycle statuses. urls defined in ../lifeCycle/lifecycle-urls.js file so we can update them in one place
