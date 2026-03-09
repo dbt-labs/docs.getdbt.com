3bb42b323926107f3ae484d3bfc0e3f4d56de5ad
Matt Shaver <60105315+matthewshaver@users.noreply.github.com>
2026-03-09 09:48:58 -0400

    Updating version

--- website/dbt-versions.js
+++ website/dbt-versions.js
@@ -15,13 +15,8 @@
  */
 exports.versions = [
   {
-    version: "2.1",
-    customDisplay: "dbt platform (Latest Fusion)",
-    isPrerelease: true,
-  },
-  {
     version: "2.0",
-    customDisplay: "Fusion CLI + extension",
+    customDisplay: "dbt Fusion engine (Latest)",
     isPrerelease: true,
   },
   {
