7d3ad1b7fdb5d0ce87e69b84896935598d404231
runleonarun <leona.campbell@dbtlabs.com>
2026-03-13 15:14:50 -0700

    chore: remove erroneous theme and config changes from branch

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -371,6 +371,11 @@ var siteSettings = {
     [
       "@signalwire/docusaurus-plugin-llms-txt",
       {
+        generate: {
+          enableMarkdownFiles: true,
+          enableLlmsFullTxt: true,
+          relativePaths: false,
+        },
         processing: {
           beforeDefaultRehypePlugins: [rehypeCodeLanguage],
         },
