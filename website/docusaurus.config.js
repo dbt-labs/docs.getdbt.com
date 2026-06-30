dcb4af7c02f6fa46ab5ef504f34b71632275af8c
Leona B. Campbell <3880403+runleonarun@users.noreply.github.com>
2026-06-29 17:17:48 -0700

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -88,8 +88,7 @@ var siteSettings = {
     },
     announcementBar: {
       id: "dbt-state-july-2026-webinar",
-      content:
-      "dbt State: Build what's changed, skip what hasn't. Join us for a live virtual event on July 15th to learn how to save on warehouse compute costs!",
+      content: "dbt State: Build what's changed, skip what hasn't. Join us for a live virtual event on July 15th to learn how to save on warehouse compute costs!",
       isCloseable: true,
     },
     announcementBarActive: true,
